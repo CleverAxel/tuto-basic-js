9a8d0ffa7c58b513f043a461db2aa4ed70e0becb
Axel <clever.axel95@gmail.com>
2023-02-07 13:01:01 +0100

    first commit

---
+++ methodString.js
@@ -0,0 +1,21 @@
+let txt = "pomme";
+let charArray = ["p","o","m","m","e"];
+
+//mêmê chose
+// console.log(txt.charAt(0))
+// console.log(txt[0])
+
+// console.log(txt.toUpperCase())
+// console.log(txt.toLowerCase())
+
+//recherche une partie dans une chaine de charactère
+//console.log(txt.includes("mm"));
+
+//omme
+// console.log(txt.substring(1))
+
+//po
+// console.log(txt.substring(0, 2));
+
+//strlen en PHP, retourne le nombre de lettre dans la chaine de charactères;
+// console.log(txt.length);
---
+++ script.js
@@ -0,0 +1,51 @@
+
+//mettre la selection en commentaire = CTRL + K + C
+//defaire les commentaires de la sélection = CTRL + K + U
+
+//afficher un message dans la console;
+// console.log("hello world");
+// console.error("rouge")
+// console.info("jaune")
+// console.warn("jaune")
+
+//alerte
+// window.alert("alerte message");
+
+//type nbr = 15, -5, 0.2, -0.2.
+let nbr = 5;
+
+//type string
+let string = "chaine";
+
+//bool = true false
+let bool = true;
+
+//array ou tableau
+let array = [1,2,3,4];
+let stringArray = ["", "", ""];
+let boolArray = [true, false, true];
+
+//object type
+let object = {
+    nbr : 5,
+    txt : "string",
+    bool : true
+};
+
+
+let string1 = "chaine1";
+let string2 = "chaine2";
+
+// = chaine1 chaine2
+let concatenatedString = string1 + " " + string2;
+console.log(concatenatedString);
+
+
+let nombre = 5;
+let nombreTxt = "10";
+
+//transforme nombre en STRING et concatène
+console.log(nombre + nombreTxt);
+
+//transforme nombreTxt en NUMBER et fait le calcul.
+console.log(nombre - nombreTxt);
